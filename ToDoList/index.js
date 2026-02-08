7d34276847ba9f38dfa6eab78f3b8c79d8d82bc6
anurag2580 <anuragrai2580@gmail.com>
2026-02-08 23:20:40 +0530

    #add get

--- ToDoList/index.js
+++ ToDoList/index.js
@@ -2,8 +2,20 @@ const http = require('http')
 
 const PORT = 8080 ;
 
-http.createServer((req,res) => {
-     
+const toDoList = ["Anurag Ra","Anu Rai","Vishal Rai","Rajesh Rai"]
+
+http.createServer((req,res)=> {
+    const {method,url} = req;
+    if(url === '/todos'){
+       if(method === "GET"){
+        res.writeHead(200,{"Content-Type": "text/html"})
+        res.write(toDoList.toString())
+        res.end();
+       }else{
+        res.write(501)
+       }
+    }else(url === "/") ;
+
 }).listen(PORT,()=>{
     console.log(`To Do list Running on PORT: http://localhost:${PORT}`)
 })
--- ToDoList/readme.md
+++ ToDoList/readme.md
@@ -9,4 +9,11 @@
 ## To run The Applcation 
 
    npm run dev
-   
+
+## HTTP methods
+
+GET     => To get/retrieve the data from the server
+POST    => Send the data to the server
+DELETE  => Depleting the data from the server
+PATCH   => Update certain files
+PUT     => Overwriting /Full Update
